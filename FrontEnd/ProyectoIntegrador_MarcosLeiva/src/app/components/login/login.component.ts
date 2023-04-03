8a6043100ed033d022cec46b40ffac01c0f57aad
Marcos Leiva <marcleiva623@gmail.com>
2023-04-03 11:47:15 -0300

    Agregado de los Forms

--- FrontEnd/ProyectoIntegrador_MarcosLeiva/src/app/components/login/login.component.ts
+++ FrontEnd/ProyectoIntegrador_MarcosLeiva/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group(
+      {
+        email:['',[Validators.required, Validators.email]],
+        password:['',[Validators.required, Validators.minLength(10)]],
+        deviceInfo:this.formBuilder.group(
+          {
+            // Completar
+            deviceId:[""],
+            deviceType:[""],
+            NotificationToken:[""]
+          }
+        )
+      }
+    );
+  }
 
+  get Email(){
+    return this.form.get('email');
+  }
+  get Password(){
+    return this.form.get('password');
+  }
 }
